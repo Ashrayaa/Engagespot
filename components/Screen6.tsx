205648b3eee9374c209ba237605a3dc3789b41c9
Ashrayaa <mailtoashrayaa@gmail.com>
2022-11-01 23:42:41 +0530

    changes added

--- components/Screen6.tsx
+++ components/Screen6.tsx
@@ -1,4 +1,9 @@
 import React from "react";
+import Image from "next/image";
+import Mail from "/public/mail.svg";
+import Twilio from "/public/twilio.svg";
+import Amazon from "/public/amazon.svg";
+import Mailgun from "/public/mailgun.svg";
 
 type Props = {};
 
@@ -9,13 +14,25 @@ export default function Screen6({}: Props) {
         Pick channels for your messages
       </h1>
       <p className="text-center font-semibold py-4">
-        setup your first notification in 5 minutes
+        Setup your first notification in 5 minutes
       </p>
 
-      <div className="bg-litepurple">
-        <div>
-          <div>image</div>
+      <div className="bg-litepurple p-6 flex flex-col gap-14 justify-center items-center">
+        <div className="flex gap-4 font-medium text-spotpurple border p-1 pb-0 border-spotpurple rounded-xl w-28">
+          <div>
+            <Image src={Mail} alt="Mail" />
+          </div>
           <p>Mail</p>
+        </div>
+
+        <div className="flex flex-col gap-8 justify-center border border-engagepurple p-4 rounded-xl border-dotted">
+          <div className="flex gap-8">
+            <Image src={Twilio} alt="Twilio SendGrid" />
+            <Image src={Amazon} alt="Amazon" />
+          </div>
+          <div className="flex justify-center">
+            <Image src={Mailgun} alt="Mailgun" />
+          </div>
         </div>
       </div>
     </div>
