150b0b7ebbb32192357e2c8f001565f2bbf1e201
Ashrayaa <mailtoashrayaa@gmail.com>
2022-11-03 12:38:01 +0530

    Intro page ui issue fixed

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -6,14 +6,14 @@ type Props = {};
 
 export default function Screen1({}: Props) {
   return (
-    <div className="flex flex-col sm:p-4 md:p-10  mb-10 lg:mt-0 lg:p-4 lg:flex-row lg:gap-32 lg:w-[95%] sm:mb-[15%]">
-      <div className="flex flex-col p-2  gap-6 md:gap-10 lg:flex lg:flex-col lg:gap-18 lg:p-16">
-        <div className="text-3xl   font-semibold text-transparent bg-clip-text bg-gradient-to-r from-spotpurple to-engagepurple sm:mt-14 mt-6 lg:text-6xl sm:text-6xl sm:justify-center sm:items-center  lg:flex lg:flex-col lg:gap-3">
+    <div className="flex flex-col sm:p-4 md:p-10 mb-10 lg:flex-row sm:mb-[15%] lg:gap-32 lg:mb-2 xl:gap-44 xl:mb-0">
+      <div className="flex flex-col p-2 xl:p-0 gap-6 md:gap-10 xl:mx-10 xl:gap-12">
+        <div className="text-3xl xl:text-5xl xl:leading-snug xl:font-bold font-semibold text-transparent bg-clip-text bg-gradient-to-r from-spotpurple to-engagepurple sm:mt-14 mt-6 lg:text-5xl sm:text-6xl sm:justify-center sm:items-center">
           <h1>Build Notification </h1>
           <h1>Infrastructure for </h1>
           <h1>your Product</h1>
         </div>
-        <div className="font-medium text-sm sm:text-lg md:text-xl text-engagepurple lg:flex lg:flex-col lg:text-lg">
+        <div className="font-medium text-sm sm:text-lg md:text-xl text-engagepurple lg:text-sm lg:mr-8 xl:-mr-16 xl:text-lg">
           <p>
             Say goodbye to building and maintaining a notification
             micro-service. Flexible, reliable notifications infrastructure
@@ -22,13 +22,14 @@ export default function Screen1({}: Props) {
           {/* <p> Use our APIs to engage users, power cross-channel
           workflows, and manage notification preferences.</p> */}
         </div>
-        <div className="sm:flex lg:flex-row sm:gap-10 flex flex-col gap-6">
+        <div className="lg:flex-row sm:gap-10 flex flex-col gap-6 lg:justify-center lg:items-center">
           <input
-            className="p-4 sm:p-8 sm:text-3xl px-14 font-semibold text-xl sm:ml-0 relative border border-engagepurple rounded-lg"
+            className="p-4 lg:text-2xl xl:py-6 xl:pr-48 sm:p-8 sm:text-3xl px-14 lg:pr-36 lg: font-semibold text-xl relative border border-engagepurple rounded-lg"
             type="textbox"
             placeholder="Enter your mail id"
-          ></input>
-          <button className="text-white p-4 sm:p-8 bg-gradient-to-r from-spotpurple to-engagepurple  font-semibold rounded-lg text-lg sm:text-2xl text-center">
+          >
+          </input>
+          <button className="text-white p-4 lg:p-4 sm:p-8 bg-gradient-to-r from-spotpurple to-engagepurple lg:absolute font-medium rounded-lg text-lg sm:text-2xl text-center lg:ml-72 xl:ml-[25%] 2xl:ml-[22%]">
             Get Started
           </button>
         </div>
@@ -38,7 +39,7 @@ export default function Screen1({}: Props) {
         </button> */}
       </div>
 
-      <div className="sm:mt-20">
+      <div className="sm:mt-20 lg:-ml-32 ">
         <Image src={Displayimage} alt="displayimage"/>
       </div>
     </div>
